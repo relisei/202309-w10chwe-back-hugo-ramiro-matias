61a87421bc9e7ad4ffaa478b76cfe7ba96a6c392
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-18 10:32:08 +0100

    Add MechaStructure types

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 import { app } from "./app.js";
 import pingRouter from "../features/ping/router/pingRouter.js";
 
-app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.json());
 app.use("/", pingRouter);
