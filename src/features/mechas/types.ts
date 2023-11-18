94e3c843d9d7a73f3b3783887b480d991ecc3f52
Matias Risco <matiasriscolagos@gmail.com>
2023-11-18 13:01:47 +0100

    Add MechasController in mechas

--- src/features/mechas/types.ts
+++ src/features/mechas/types.ts
@@ -1,11 +1,17 @@
 import { type Request } from "express";
-
-export interface MechaStructure {
-  _id: string;
+interface MechaBaseStructure {
   name: string;
   image: string;
   characteristics: {
     speed: number;
     strength: number;
   };
+}
+
+export interface MechaApiStructure extends MechaBaseStructure {
+  _id: string;
+}
+
+export interface MechaStructure extends MechaBaseStructure {
+  id: string;
 }
