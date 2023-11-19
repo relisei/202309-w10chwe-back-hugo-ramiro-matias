f442f19b0212b7b533d27114cff2b2e6c2c22bae
Matias Risco <matiasriscolagos@gmail.com>
2023-11-19 15:39:32 +0100

    Add fix security of cords

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,8 +4,9 @@ import { app } from "./app.js";
 import pingRouter from "../features/ping/router/pingRouter.js";
 import mechasRouter from "../features/mechas/router/mechasRouter.js";
 import cors from "cors";
+import { corsOptions } from "./utils/cords.js";
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/mechas", mechasRouter);
